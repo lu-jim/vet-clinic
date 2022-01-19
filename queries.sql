351b495c0fb19b30a8270d798062ad485deaa1f2
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-01-19 16:19:56 -0500

    Enforce consistent spacing

--- queries.sql
+++ queries.sql
@@ -1,10 +1,43 @@
 /*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name LIKE '%mon';
-SELECT name from animals WHERE date_of_birth < '2020-01-01' AND date_of_birth > '2015-12-31';
-SELECT name from animals WHERE neutered = TRUE AND escape_attempts < 3;
-SELECT date_of_birth from animals WHERE name = 'Agumon' OR name = 'Pikachu';
-SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
-SELECT * from animals WHERE neutered = TRUE;
-SELECT * from animals WHERE name != 'Gabumon';
-SELECT * from animals WHERE weight_kg <= 17.3 AND weight_kg > 10.4;
+SELECT *
+from animals
+WHERE name LIKE '%mon';
+SELECT name
+from animals
+WHERE date_of_birth < '2020-01-01'
+  AND date_of_birth > '2015-12-31';
+SELECT name
+from animals
+WHERE neutered = TRUE
+  AND escape_attempts < 3;
+SELECT date_of_birth
+from animals
+WHERE name = 'Agumon'
+  OR name = 'Pikachu';
+SELECT name,
+  escape_attempts
+from animals
+WHERE weight_kg > 10.5;
+SELECT *
+from animals
+WHERE neutered = TRUE;
+SELECT *
+from animals
+WHERE name != 'Gabumon';
+SELECT *
+from animals
+WHERE weight_kg <= 17.3
+  AND weight_kg > 10.4;
+/* Query and update animals table */
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+ROLLBACK;
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species = 'pokemon'
+WHERE species = '';
+COMMIT;
