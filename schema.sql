851c438e4feeb5ad474fbdc5f67f3f0c37a8ae3c
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-01-19 13:24:53 -0500

    Address PR feedback - make id autoincrementing primary key

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 CREATE TABLE animals (
-    id int,
+    id SERIAL PRIMARY KEY,
     name varchar(80),
     date_of_birth date,
     escape_attempts int,
