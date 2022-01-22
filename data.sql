a55632166de185bbb2e617d91810b8a46cbcfdbb
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-01-21 19:30:41 -0500

    Add specialization data

--- data.sql
+++ data.sql
@@ -123,12 +123,13 @@ SET owner_id = 5
 WHERE name = 'Angemon'
   OR name = 'Boarmon';
 /* Add "join table" for visits */
-INSERT INTO vets(
-    name,
-    age,
-    date_of_graduation
-  )
+INSERT INTO vets(name, age, date_of_graduation)
 VALUES ('William Tatcher', 45, '2000-04-23'),
   ('Maisy Smith', 26, '2019-01-17'),
   ('Stephanie Mendez', 64, '1981-05-04'),
   ('Jack Harkness', 38, '2008-06-08');
+INSERT INTO specializations(vet_id, animal_id)
+VALUES (1, 1),
+  (3, 1),
+  (3, 2),
+  (4, 2);
